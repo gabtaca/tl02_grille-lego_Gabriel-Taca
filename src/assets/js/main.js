0bb04d10ba7a8580fb29eb521b738f21d0c50697
Gabtaca <gabrieltaca117@gmail.com>
2024-07-02 09:44:38 -0400

    1st

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,22 +1,26 @@
 // main.js
 import '../css/index.sass'
 import '../css/style.css'
-import '../js/age.js'
-import '../js/caroussel.js'
 
 
-const formData = new FormData();
-formData.append('photo', photoInput.files[0]);
-formData.append('title', 'My Photo');
-formData.append('activity', 'My Activity');
+document.addEventListener("DOMContentLoaded", () => {
+  const toggleButton = document.getElementById("toggleButton");
+  const body = document.getElementById("body");
+  const card = document.getElementById("card");
 
-try {
-  const response = await fetch('/api/photos/upload-photo', {
-    method: 'POST',
-    body: formData
-  });
-  const data = await response.json();
-  console.log(data);
-} catch (error) {
-  console.error(error);
-}
+  toggleButton.addEventListener("click", () =>{
+    
+    card.classList.toggle("bg-black")
+    card.classList.toggle("bg-pink-500")
+    body.classList.toggle("bg-cyan-200")
+    body.classList.toggle("bg-black")
+  })
+})
+
+const card = document.createElement("div");
+card.classList = "card-body, bg-black, w-[300px], h-[300px] text-white";
+
+const content = `
+<div class-"card>
+<p>300px / 300px</p>
+</div>`
